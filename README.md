2addbf065f392b2508f9ebe8c394cddff033a8a5
AAweidai <dw1253464613@gmail.com>
2022-05-09 20:04:46 +0800

    Add dogecoin api

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dogecoin-ios-api
+
+For the convenience of use, the dogecoin xcframework is packaged into a package. After that, the swift project can use the dogecoin api only by introducing this repository.
---
+++ Sources/Dogecoin.xcframework/ios-x86_64-simulator/Dogecoin.framework/Headers/Dogecoin.h
@@ -0,0 +1,18 @@
+//
+//  Dogecoin.h
+//  Dogecoin
+//
+//  Created by daiwei on 2022/5/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Dogecoin.
+FOUNDATION_EXPORT double DogecoinVersionNumber;
+
+//! Project version string for Dogecoin.
+FOUNDATION_EXPORT const unsigned char DogecoinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dogecoin/PublicHeader.h>
+
+#import <Dogecoin/DogecoinHeader.h>
